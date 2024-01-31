19af3f5d04bcb11ecc1fb417245961244f6a6e2f
DEV_RURU <ssez13@naver.com>
2024-01-31 18:05:17 +0900

    Docs(ing): 폴더 구조 설정 중
    code > week 폴더들로 나뉠 수 있게 설정

---
+++ Park-Se-Jeong/code/week1/anagram.js
@@ -0,0 +1,11 @@
+/*
+* Given two strings, write a function to determine if the
+second string is an anagram of the first. An anagram is
+a word, phras, or name formed by rearranging the
+letters of another, such as cinema, formed from iceman.
+*/
+
+
+function validAnagram() {
+
+}
